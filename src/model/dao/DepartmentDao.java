ccaa11cb097aec744031a590d71d0c8da86f0e4c
Darci Jorge <aquarius.cd@gmail.com>
2025-01-15 20:55:35 -0300

    Adicionadas interfaces Department e Seller

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,14 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
 public interface DepartmentDao {
 
+	void insert(Deprecated obj);
+	void update(Deprecated obj);
+	void deleteById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
 }
