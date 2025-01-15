2261ea1466bace6894e0d31379ca2c156446bfc7
Darci Jorge <aquarius.cd@gmail.com>
2025-01-15 20:38:29 -0300

    Incluida entitie Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Department obj = new Department(1, "Books");
-		System.err.println(obj.toString());
+		Department department = new Department(1, "Books");
+	
+		Seller seller = new Seller(1, "Darci Jorge", "aquarius.cd@gmail.com", new Date(), 1500.0, department);
 		
-		
+		System.out.println(seller.toString());
 	}
 
 }
