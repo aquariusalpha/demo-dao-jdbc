62260a585b2b04bca8b45d18f75af30520e34eca
Darci Jorge <aquarius.cd@gmail.com>
2025-01-16 21:40:24 -0300

    Implementando findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,13 +11,15 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Department department = new Department(1, "Books");
-	
-		Seller seller = new Seller(1, "Darci Jorge", "aquarius.cd@gmail.com", new Date(), 1500.0, department);
-
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
+		Seller seller = sellerDao.findById(3);
+		
 		System.out.println(seller);
+				
+		
+		
+		
 	}
 
 }
