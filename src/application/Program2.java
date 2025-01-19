769dc6876a9249ada7d8ed613ef16c7898a64b1d
Darci Jorge <aquarius.cd@gmail.com>
2025-01-19 18:43:47 -0300

    UPDATE AND DELETE department

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
 
@@ -26,7 +27,6 @@ public class Program2 {
 		}else{
 			System.out.println(department);		
 		}	
-	
 
 		System.out.println("=== TESTE 2 findAll ===");
 		List<Department> listDepartmentAll = departmentDao.findAll();
@@ -35,6 +35,24 @@ public class Program2 {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== TEST 3: insert =======");
+		Department newDepartment = new Department(null, "Music");
+		departmentDao.insert(newDepartment);
+		System.out.println("Inserted! New id: " + newDepartment.getId());	
+		
+		System.out.println("\n=== TEST 4: update =======");
+		Department dep2 = departmentDao.findById(1);
+		dep2.setName("Food");
+		departmentDao.update(dep2);
+		System.out.println("Update completed");
+
+		System.out.println("=== TESTE 5 DELETE ===");
+		System.out.print("Enter Id for deletion test: ");
+		Integer idDelete = sc.nextInt();
+		departmentDao.deleteById(idDelete);
+		System.out.println("Delete completed");
+		
+
 		
 		
 		sc.close();
